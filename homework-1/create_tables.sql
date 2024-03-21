5da2a5274d97b491082b21d2983fc5433e97438a
AlCher38 <154320556+AlCher38@users.noreply.github.com>
2024-03-21 12:41:27 +0800

    Create BD

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,22 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE employees
+(
+    post_id int PRIMARY KEY,
+    title varchar(100) NOT NULL,
+    content text
+);
+
+CREATE TABLE customers
+(
+    post_id int PRIMARY KEY,
+    title varchar(100) NOT NULL,
+    content text
+);
+
+CREATE TABLE orders
+(
+    post_id int PRIMARY KEY,
+    title varchar(100) NOT NULL,
+    content text
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,8 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+import psycopg2
+
+
+def create_suppliers_table(cur):
+    """Создает таблицу suppliers."""
+    pass
