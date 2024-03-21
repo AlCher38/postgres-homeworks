119dfc8479acb8e3adb0e0a28a7606a15355dd26
AlCher38 <154320556+AlCher38@users.noreply.github.com>
2024-03-21 12:43:11 +0800

    create suppliers

--- homework-1/main.py
+++ homework-1/main.py
@@ -5,4 +5,13 @@ import psycopg2
 
 def create_suppliers_table(cur):
     """Создает таблицу suppliers."""
-    pass
+    with psycopg2.connect(host="localhost", database="test", user="postgres", password="Ghjcnj038") as conn:
+        with conn.cursor() as cur:  # делает commit после выполнения
+            cur.executemany("INSERT INTO user_acoount VALUES (%s, %s)", [(9, 'John')])
+            cur.execute("SELECT * FROM user_acoount")
+
+            rows = cur.fetchall()
+            for row in rows:
+                print(row)
+
+    conn.close()
