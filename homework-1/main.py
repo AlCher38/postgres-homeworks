3efb230cc646ceb1fd1e3d3bd7c927d1be7d964d
AlCher38 <154320556+AlCher38@users.noreply.github.com>
2024-04-06 22:26:17 +0800

    Update main.py

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,17 +1,33 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
 
+import csv
 import psycopg2
 
+# Connect to the database
+conn = psycopg2.connect(database="north", user="val_k", password="1986", host="localhost", port="5432")
+cur = conn.cursor()
 
-def create_suppliers_table(cur):
-    """Создает таблицу suppliers."""
-    with psycopg2.connect(host="localhost", database="north", user="postgres", password="Ghjcnj038") as conn:
-        with conn.cursor() as cur:  # делает commit после выполнения
-            cur.executemany("INSERT INTO user_acoount VALUES (%s, %s)", [(9, 'John')])
-            cur.execute("SELECT * FROM user_acoount")
+# Read and insert data from customers_data.csv
+with open('north_data/customers_data.csv', 'r') as file:
+    reader = csv.reader(file)
+    next(reader)  # Skip the header row
+    for row in reader:
+        cur.execute("INSERT INTO customers VALUES (%s, %s, %s)", (row[0], row[1], row[2]))
 
-            rows = cur.fetchall()
-            for row in rows:
-                print(row)
+# Read and insert data from employees_data.csv
+with open('north_data/employees_data.csv', 'r') as file:
+    reader = csv.reader(file)
+    next(reader)  # Skip the header row
+    for row in reader:
+        cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", (row[0], row[1], row[2], row[3], row[4], row[5]))
 
-    conn.close()
+# Read and insert data from orders_data.csv
+with open('north_data/orders_data.csv', 'r') as file:
+    reader = csv.reader(file)
+    next(reader)  # Skip the header row
+    for row in reader:
+        cur.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)", (row[0], row[1], row[2], row[3], row[4]))
+
+# Commit the changes and close the connection
+conn.commit()
+conn.close()
